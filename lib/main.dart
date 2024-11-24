1d84938d2fc53e91f357901d90851bf888d47183
GzarGmez <97687278+GzarGmez@users.noreply.github.com>
2024-11-23 20:22:20 -0600

    Actualizaciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
 import 'package:integrador/Pages/Register_User.dart';
 import 'package:integrador/Pages/home_admin.dart';
 import 'package:integrador/Pages/home_page.dart';
@@ -6,7 +6,11 @@ import 'package:integrador/Pages/home_user.dart';
 import 'package:integrador/Pages/QRScanPage.dart';
 import 'pages/user_view.dart';
 import 'pages/admin_view.dart';
+import 'package:integrador/pages/eventos_screen.dart';
+import 'package:integrador/pages/splash_screen.dart';  // Importa el archivo SplashScreen
 
+
+// 1. Aquí configuramos el main para que muestre el SplashScreen primero.
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -16,15 +20,17 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Navigation Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/splash', // Cambié la ruta inicial para que sea el Splash
       routes: {
+        '/splash': (context) => SplashScreen(), // Ruta para el Splash
         '/': (context) => HomePage(),
         '/user': (context) => UserView(),
         '/admin': (context) => AdminView(),
         '/register': (context) => RegisterView(),
         '/userDashboard': (context) => UserDashboardView(),
-        '/homeAdmin': (context) =>  HomeAdminView(),
-        '/qrscan': (context) => QRScanPage(), // New route for admin home
+        '/homeAdmin': (context) => HomeAdminView(),
+        '/qrscan': (context) => QRScanPage(),
+        '/events': (context) => EventosScreen(), // Nueva ruta para la pantalla de eventos
       },
     );
   }
